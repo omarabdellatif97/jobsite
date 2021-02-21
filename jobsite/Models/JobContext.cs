f1d8b627360fb9ab660c58bd6d1bd3e01e99c8db
Omar Hesham <omar_abdellatif97@hotmail.com>
2021-02-21 21:14:28 +0200

    Add project files.

---
+++ jobsite/Models/JobContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jobsite.Models
+{
+    public class JobContext: DbContext
+    {
+        public JobContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
