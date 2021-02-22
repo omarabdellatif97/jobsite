421e39d1916879ed202cbe60ee6345cea6bafe7c
Moamen Soroor <moamensoroor@gmail.com>
2021-02-23 00:05:00 +0200

    add basic interfaces

---
+++ jobsite/Services/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using jobsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jobsite.Services
+{
+    public interface IRepository<T> : IDisposable
+        where T : EntityBase
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Insert(T entity);
+        void Delete(int id);
+        void Update(T entity);
+        void Save();
+    }
+}
